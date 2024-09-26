fa8d86f68e9caa8dfd35a537cc83ffc57c83ff0a
kochenov <kochenof@yandex.ru>
2024-09-26 17:51:05 +0300

    Представления

--- recipe_site/recipes/views.py
+++ recipe_site/recipes/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Recipe
+import random
 
-# Create your views here.
+
+def home(request):
+    recipes = Recipe.objects.all()
+    random_recipes = random.sample(list(recipes), 5) if recipes.count() >= 5 else recipes
+    return render(request, 'home.html', {'recipes': random_recipes})
+
+
+def recipe_detail(request, recipe_id):
+    recipe = get_object_or_404(Recipe, id=recipe_id)
+    return render(request, 'recipe_detail.html', {'recipe': recipe})
