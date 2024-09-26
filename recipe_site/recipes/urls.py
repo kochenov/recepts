83ff12492788b9a61d3ebca8c231ccf5b0e8c884
kochenov <kochenof@yandex.ru>
2024-09-26 17:52:53 +0300

    Маршруты

---
+++ recipe_site/recipes/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('recipe/<int:recipe_id>/', views.recipe_detail, name='recipe_detail'),
+]
