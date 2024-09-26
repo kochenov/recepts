1e5df3c2da0f8746fedc703afe61a799422e4701
kochenov <kochenof@yandex.ru>
2024-09-26 17:44:13 +0300

    Модели

--- recipe_site/recipes/models.py
+++ recipe_site/recipes/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    steps = models.TextField()
+    cooking_time = models.PositiveIntegerField(help_text="Время в минутах")
+    image = models.ImageField(upload_to='recipes/')
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ManyToManyField(Category)
+
+    def __str__(self):
+        return self.title
